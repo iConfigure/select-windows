1df7a7ef500c64d4b4029c20c350b401ed3ae02f
Joppe44 <106473010+Joppe44@users.noreply.github.com>
2025-10-03 10:11:41 +0200

    changed things lol

--- main.js
+++ main.js
@@ -2,26 +2,26 @@
 console.log("loaded");
 
 function scrollToConfigurator(event) {
-  event.preventDefault();
-  const el = document.getElementById("configurator");
-  el.scrollIntoView({ behavior: "smooth", block: "start" });
+    event.preventDefault();
+    const el = document.getElementById("configurator");
+    el.scrollIntoView({ behavior: "smooth", block: "start" });
 }
 
 function updateIframePointerEvents() {
-  const iframe = document.getElementById("iConfigure");
-  const configurator = document.getElementById("configurator");
-  const rect = iframe.getBoundingClientRect();
+    const iframe = document.getElementById("iConfigure");
+    const configurator = document.getElementById("configurator");
+    const rect = iframe.getBoundingClientRect();
 
-  if (rect.top <= 1) {
-    // iframe.style.pointerEvents = "auto";
-    const hero = document.getElementById("hero");
-    if (hero) hero.remove();
-    if (window.scrollY < configurator.offsetTop) {
-      configurator.scrollIntoView({ behavior: "instant", block: "start" });
+    if (rect.top <= 1) {
+        // iframe.style.pointerEvents = "auto";
+        const hero = document.getElementById("hero");
+        if (hero) hero.remove();
+        if (window.scrollY < configurator.offsetTop) {
+            configurator.scrollIntoView({ behavior: "instant", block: "start" });
+        }
+    } else {
+        // iframe.style.pointerEvents = "none";
     }
-  } else {
-    // iframe.style.pointerEvents = "none";
-  }
 }
 
 iframe = document.getElementById("iConfigure");
@@ -33,28 +33,29 @@ window.addEventListener("load", updateIframePointerEvents);
 // ----- existing config -----
 let preConfig;
 const defaults = {
-  product: "6e34fc1b-a65e-431a-b371-f6fd80f9bc66",
-  type_samenstelling: "geen_glas",
-  draairichting: "draairichting_links",
-  aantal_ruiten: "0",
-  hoogte_sparing: "230",
-  breedte_sparing_1_element: "90",
-  textuur_db: "textuur_pvc_db",
-  kleur_db: "9001_db",
-  textuur_kb: "textuur_pvc_kb",
-  kleur_kb: "9001_kb",
-  textuur_di: "textuur_pvc_di",
-  kleur_di: "9001_di",
-  model: "basic",
-  deur_lijst: "met",
-  greep: "740_6001",
-  kleur_greep: "kleur_greep_rvs",
-  brievenbus: "homebox",
-  profiel: "profiel_facet",
-  profiel_afwerking: "verstek",
-  dorpel: "dorpel_zwart",
-  glas_textuur: "transparant",
-  active_step: "3",
+    product: "6e34fc1b-a65e-431a-b371-f6fd80f9bc66",
+    type_samenstelling: "geen_glas",
+    draairichting: "draairichting_rechts",
+    aantal_ruiten: "0",
+    hoogte_sparing: "230",
+    breedte_sparing_1_element: "90",
+    model: "basic",
+    deur_lijst: "met",
+    greep: "740_6001",
+    kleur_greep: "kleur_greep_alluminium",
+    brievenbus: "geen_brievenbus",
+    profiel: "profiel_accent",
+    profiel_afwerking: "verstek",
+    dorpel: "dorpel_grijs",
+    textuur_db: "textuur_pvc_db",
+    kleur_db: "9001_db",
+    textuur_kb: "textuur_pvc_kb",
+    kleur_kb: "9001_kb",
+    textuur_di: "textuur_pvc_di",
+    kleur_di: "9001_di",
+    glas_textuur: "transparant",
+    aantal_glas: "0",
+    active_step: "2",
 };
 
 const qpObj = Object.fromEntries(new URLSearchParams(location.search));
